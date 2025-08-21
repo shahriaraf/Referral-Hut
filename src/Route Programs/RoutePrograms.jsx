6a87eb27a6479d6aeb392737834fd8c7dcadb7d8
noorjahan220 <noorjahanmeem220@gmail.com>
2025-08-21 13:00:06 +0600

    create RouteProgramPage

---
+++ src/Route Programs/RoutePrograms.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RoutePrograms = () => {
+    return (
+        <div>
+            <button className='bg-red'>3p</button>
+            <button className='bg-green'>4p</button>
+            <button className='bg-blue'>6p</button>
+        </div>
+    );
+};
+
+export default RoutePrograms;
