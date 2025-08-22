df4ad6d292b9e07847b106a00d91d481223839bb
jamil9080 <md3711451@gmail.com>
2025-08-22 17:34:17 +0600

    Added new feature signup login profile page

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -4,11 +4,17 @@ import RoutePrograms from '../Route Programs/RoutePrograms';
 import OurPertners from './OurPertners';
 import WhyChooseUs from './WhyChooseUs';
 import Crypto from './Crypto';
+import Profile from '../Components/profile/Profile';
+import Signup from '../Components/login/Signup/Signup';
+import Login from '../Components/login/Signin/Login';
 
 const Home = () => {
     return (
         <div className='primary_bg_color w-full h-auto'>
                     <Banner></Banner>
+                    <Profile></Profile>
+                    <Signup></Signup>
+                    <Login></Login>
                      <WhyChooseUs></WhyChooseUs>
                      <Crypto></Crypto>
                      <OurPertners></OurPertners>
