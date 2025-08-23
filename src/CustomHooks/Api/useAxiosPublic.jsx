8623405f2eb431c012ea45f149264e2d305010ba
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-08-24 01:06:09 +0600

    creat user unique_id and save user data to database

---
+++ src/CustomHooks/Api/useAxiosPublic.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import React from 'react';
+
+const useAxiosPublic = () => {
+    const axiosPublic = axios.create({
+        baseURL : 'http://localhost:5000/'
+    })
+    return  axiosPublic;
+};
+
+export default useAxiosPublic;
