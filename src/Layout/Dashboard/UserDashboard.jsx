1306774378b57ffa8ce625165ebf68b2d4d011bf
noorjahan220 <noorjahanmeem220@gmail.com>
2025-08-25 18:25:29 +0600

    Add padding to dashboard header for better spacing

--- src/Layout/Dashboard/UserDashboard.jsx
+++ src/Layout/Dashboard/UserDashboard.jsx
@@ -23,7 +23,7 @@ const DashboardHeader = ({ user }) => {
     if (!user) return <DashboardHeaderSkeleton />;
 
     return (
-        <div className="flex items-center justify-between px-10 bg-gradient-to-r from-[#161B22] to-[#1a202c] rounded-lg shadow-lg border border-gray-800">
+        <div className="flex items-center p-6 justify-between px-10 bg-gradient-to-r from-[#161B22] to-[#1a202c] rounded-lg shadow-lg border border-gray-800">
             <div className="flex-1">
                 <h1 className="lg:text-2xl text-xl font-bold text-white">
                     Welcome back,{' '}
@@ -188,20 +188,16 @@ const { logOut } = useAuth();
             <main className="flex-1 flex flex-col overflow-hidden">
 
 
-                <div className="px-6 lg:px-10 pt-6 lg:pt-8">
+                <div className="pt-2 lg:px-10 ">
                     <DashboardHeader user={user} />
                 </div>
 
 
 
-                {/* Header Container */}
-                <div className=" pt-6 lg:pt-8 mb-8">
-                    <DashboardHeader user={user} />
-                </div>
 
                 {/* Scrollable Inner Container */}
 
-                <div ref={mainContentRef} className="flex-1 overflow-y-auto px-6 lg:px-10 pb-20 md:pb-10">
+                <div ref={mainContentRef} className="flex-1 overflow-y-auto px-6 lg:px-10 pt-4 pb-20 md:pb-10">
                     <Outlet />
                 </div>
             </main>
